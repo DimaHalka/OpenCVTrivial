e46f32e78488dd283da1fc3560aa2b9f7391fd10
Dima <dima.halka@gmail.com>
2024-05-02 17:21:37 +0300

    Comments update

--- image_morphing_dilate.py
+++ image_morphing_dilate.py
@@ -11,8 +11,10 @@ def test_key(key):
 
 
 def image_morphing_dilate(picture):
-    # Erosion: It is useful for removing small white noises.
-    # Used to detach two connected objects etc.
+    # In cases  like noise removal, erosion is followed by dilation.
+    # Because, erosion removes white noises, but it also shrinks our object.
+    # So we dilate it. Since noise is gone, they won’t come back, but our object area increases.
+    # It is also useful in joining broken parts of an object.
     kernel = np.ones((5, 5), np.uint8)
     picture = cv2.cvtColor(picture, cv2.COLOR_BGR2GRAY)
     return cv2.dilate(picture, kernel, iterations=1)
